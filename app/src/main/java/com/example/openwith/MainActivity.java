91e1ca63a796cbb8ed050648b3acf23d5a66e2e1
kaconk <harutonouciha@gmail.com>
2020-03-15 07:26:14 +0700

    set alarm

--- app/src/main/java/com/example/openwith/MainActivity.java
+++ app/src/main/java/com/example/openwith/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.openwith;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.AlarmClock;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
-    EditText Turl;
-    String url;
+    EditText Turl, Ttext, Tnumber, Tcall;
+    String url, InText, Dnumber, Cnumber, am_pm, pesan;
+    int hour, minutes;
+    TimePicker picker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,5 +37,76 @@ public class MainActivity extends AppCompatActivity {
             startActivity(openweb);
         }
 
+    }
+
+    public void OpenText(View view) {
+        Ttext = findViewById(R.id.inputext);
+        InText = Ttext.getText().toString();
+
+        Intent sendtext = new Intent(Intent.ACTION_SEND);
+        sendtext.putExtra(Intent.EXTRA_TEXT,InText);
+        sendtext.setType("text/plain");
+
+        if (sendtext.resolveActivity(getPackageManager()) != null){
+            startActivity(sendtext);
+        }
+    }
+
+    public void OpenDial(View view) {
+        Tnumber = findViewById(R.id.phonenumber);
+        Dnumber = Tnumber.getText().toString();
+        Uri tel = Uri.parse("tel:"+Dnumber);
+        Intent phonedial = new Intent(Intent.ACTION_DIAL, tel);
+
+        if (phonedial.resolveActivity(getPackageManager()) != null){
+            startActivity(phonedial);
+        }
+    }
+
+    public void OpenCall(View view) {
+        Tcall = findViewById(R.id.numbercall);
+        Cnumber = Tcall.getText().toString();
+        Uri call = Uri.parse("tel:"+Cnumber);
+        Intent callnumber = new Intent(Intent.ACTION_CALL,call);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+        }else {
+            if (callnumber.resolveActivity(getPackageManager()) != null) {
+                startActivity(callnumber);
+            }
+        }
+
+    }
+
+    public void SetAlarm(View view) {
+
+        picker = findViewById(R.id.datePicker1);
+        if (Build.VERSION.SDK_INT >= 23 ){
+            hour = picker.getHour();
+            minutes = picker.getMinute();
+        }
+        else{
+            hour = picker.getCurrentHour();
+            minutes= picker.getCurrentMinute();
+        }
+        if(hour > 12) {
+            am_pm = "PM";
+            hour = hour - 12;
+        }
+        else
+        {
+            am_pm="AM";
+        }
+
+        pesan = "test";
+        Intent setalarm = new Intent(AlarmClock.ACTION_SET_ALARM)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, pesan)
+                .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                .putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+        if (setalarm.resolveActivity(getPackageManager()) != null) {
+            startActivity(setalarm);
+        }
     }
 }
